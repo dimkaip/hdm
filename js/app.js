4ca15d8b313daaa5540fbc257a7c538b0e307453
dimkaip <dim.kaipis@gmail.com>
2026-04-24 00:53:50 +0300

    first

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('CivicGraph app.js loaded');
+
+  if (typeof graphData !== 'undefined') {
+    console.log('Graph data available:', graphData);
+  }
+});
